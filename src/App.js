5547c7140c06738004bb72cc01c279197e58886b
Himanshu Shekhar <111629292+Himanshu-Shekhar-2k25@users.noreply.github.com>
2023-07-27 23:26:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header, { Landing_Page , CreateAcc, Product_Navbar, Product_All, SlideSho
 
 function App()
 {
-    return (<BrowserRouter basename="/">
+    return (<BrowserRouter>
         <Routes>
 
         <Route path='/' element={<div className="headerContainer"><Header/><Landing_Page/></div>}/>
@@ -26,5 +26,3 @@ function App()
     )
 }
 export default App;
-
-        
